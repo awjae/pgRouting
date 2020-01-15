6440d335243583dbf0523dffed851573b8a0c8ee
awjae <djwotmd@gmail.com>
2020-01-15 17:54:25 +0900

    set post request

--- main.js
+++ main.js
@@ -46,4 +46,33 @@ server.get('/list.do', function(req, res) {
     
 });
 
+server.post('/postList.do', function (req, res) {
+    console.log('/postList.do 요청됨');
+    console.log('PARAMS');
+    console.dir(req.body);
+
+    var paramId = req.body.id;
+    var paramPw = req.body.pw;
+
+    var sql = "select id, pw from test.users where id = ? and pw = ?";
+
+    var dbParams = [paramId, paramPw];
+    
+    database.query(res, sql, dbParams, function(err, rows) {
+        
+        req.session.user= {
+            id: rows[0].id,
+            name: rows[0].name
+        };
+        
+        if (rows.length > 0) {
+            util.sendResponse(res, rows);
+        } else {
+            util.sendResponse(res, []);
+        } 
+        
+    });
+
+});
+
 server.start();
